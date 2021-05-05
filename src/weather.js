0c7e2aa77360d7f65f55bb88038ef432d4a8d121
Tiara Feaster <tfeaster21@gmail.com>
2021-05-04 20:31:32 -0400

    showed current temperature

--- src/weather.js
+++ src/weather.js
@@ -1,8 +1,18 @@
 function showTemperature(response) {
-    console.log(response.data.main.temp);
+    let temperature = document.querySelector("#temperature");
+    let city = document.querySelector(".city");
+    let description = document.querySelector("#description");
+    let humidity = document.querySelector("#humidity");
+    let wind = document.querySelector("#wind");
+    city.innerHTML= response.data.name
+    temperature.innerHTML = Math.round(response.data.main.temp);
+    description.innerHTML= response.data.weather[0].description;
+    humidity.innerHTML=response.data.main.humidity;
+    wind.innerHTML=Math.round(response.data.wind.speed);
+    console.log(response.data)
 }
 
 let apiKey = "5423fee45fccae4c3cd5d7b43daf88ad";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=London&appid=${apiKey}&units=imperial`;
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Orlando&appid=${apiKey}&units=imperial`;
 
 axios.get(apiUrl).then(showTemperature);
